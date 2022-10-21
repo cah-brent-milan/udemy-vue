600bbee3ee684c6ed01317114f428686f9873da8
cah-brent-milan <brent.milan@cardinalhealth.com>
2022-10-21 14:37:13 -0400

    battle log

--- game/app.js
+++ game/app.js
@@ -7,47 +7,84 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 1
+            currentRound: 1,
+            logMessages: [],
+            testMode: false,
         }
     },
     computed: {
         displayPlayerHealth() {
+            if(this.playerHealth < 0) { this.playerHealth = 0 };
             return { width: this.playerHealth + '%'};
         },
         displayMonsterHealth() {
+            if(this.monsterHealth < 0) { this.monsterHealth = 0};
             return { width: this.monsterHealth + '%'};
         },
         isSpecialAttackAvailable() {
             return  this.currentRound % 3 !== 0
         },
         isGameOver() {
-            return !this.playerHealth > 0 || !this.monsterHealth > 0;
+            return this.isWin || this.isLoss || this.isDraw;
+        },
+        isWin() {
+            return this.playerHealth > 0 && this.monsterHealth <= 0;
+        },
+        isLoss() {
+            return this.playerHealth <= 0 && this.monsterHealth > 0;
+        },
+        isDraw() {
+            return this.playerHealth <= 0 && this.monsterHealth <= 0;
         }
     },
     methods: {
         attack() {
             const attackValue = getRandom(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLog('Player', 'attacks', attackValue);
             this.reprisal();
             this.currentRound++;
         },
         specialAttack() {
-            this.monsterHealth -= getRandom(10, 25);
+            const attackValue = getRandom(10, 25);
+            this.monsterHealth -= attackValue;
+            this.addLog('Player', 'special attacks', attackValue);
             this.reprisal();
             this.currentRound++;
         },
         reprisal() {
             const attackValue = getRandom(8, 15);
             this.playerHealth -= attackValue;
+            this.addLog('Monster', 'retaliates', attackValue);
         },
         heal() {
-            const healing = getRandom(5, 10);
+            const healing = getRandom(8, 20);
             this.playerHealth += healing;
             if (this.playerHealth > 100) { this.playerHealth = 100 }; 
+            this.addLog('Player', 'heals', healing);
+            this.reprisal();
             this.currentRound++;
         },
         surrender() {
             this.playerHealth = 0;
+        },
+        newGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 1;
+            this.logMessages = [];
+        },
+        forceDraw() {
+            this.playerHealth = 0;
+            this.monsterHealth = 0;
+        },
+        addLog(who, action, value) {
+            this.logMessages.unshift(
+                { 
+                    actionBy: who, 
+                    actionType: action, 
+                    actionValue: value
+                });
         }
     }
 });
